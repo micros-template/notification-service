dc31da5fe7e862555d5778f7f04ab81dc400d010
daffadon <daffaputranarendra9@gmail.com>
2025-07-14 15:28:29 +0700

    chore(ci): change ci script

--- bin/build.sh
+++ bin/build.sh
@@ -12,8 +12,17 @@ echo "Building Docker image for $service_name:$VERSION" >/dev/stderr
 docker build -t "$service_name:$VERSION" --build-arg BIN_NAME=$service_name -f Dockerfile .
 
 registry_image="$CI_REGISTRY_IMAGE:$VERSION"
+echo "$CI_JOB_TOKEN" | docker login -u gitlab-ci-token --password-stdin "$CI_REGISTRY"
+
 echo "Tagging image as $registry_image" >/dev/stderr
 docker tag "$service_name:$VERSION" "$registry_image"
-
-echo "$CI_JOB_TOKEN" | docker login -u gitlab-ci-token --password-stdin "$CI_REGISTRY"
 docker push "$registry_image"
+
+echo "Tagging image as $registry_image" >/dev/stderr
+docker tag "$service_name:$VERSION" "$CI_REGISTRY_IMAGE:latest"
+docker push "$CI_REGISTRY_IMAGE:latest"
+
+echo "Removing local image and tagged image" >/dev/stderr
+docker rmi "$service_name:$VERSION"
+docker rmi "$registry_image"
+docker rmi "$CI_REGISTRY_IMAGE:latest"
