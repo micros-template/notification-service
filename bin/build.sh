cfa4cf23277c44066cc9ebd56370b8e66fef4b5f
daffadon <daffaputranarendra9@gmail.com>
2025-07-14 14:45:04 +0700

    chore(ci): change ci script

--- bin/build.sh
+++ bin/build.sh
@@ -7,7 +7,7 @@ service_name="notification_service"
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o "./dist/$service_name" ../cmd/
 wait
 
-VERSION=$(cat ../VERSION)
+VERSION=$(cat VERSION)
 echo "Building Docker image for $service_name:$VERSION" >/dev/stderr
 docker build -t "$service_name:$VERSION" --build-arg BIN_NAME=$service_name -f ../Dockerfile ../
 
